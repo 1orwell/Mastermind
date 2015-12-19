d78841aadd5b1067d9c904e101ebd53cd47ef16b
Elise Ratcliffe <elise@eliseratcliffe.co.uk>
2015-12-19 19:53:13 +0000

    Completed cherker class and also added tests.

--- Checker.java
+++ Checker.java
@@ -1,60 +1,43 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class Checker {
-
-	public static void main(String args[]) {
-		int counter = 0;
+    public static List<Integer> check(ArrayList<String> code, ArrayList<String> guess) {
+	    int index = 0;
 		//ArrayList<String> code = Code.getCode();
 		//ArrayList<String> guess = Guess.getGuess();
-		ArrayList<String> code = new ArrayList<String>();
-		code.add("blue");
-		code.add("red");
-		code.add("red");
-		code.add("red");
-		code.add("red");
-		code.add("red");
-		code.add("red");
-		code.add("red");
-		ArrayList<String> guess = new ArrayList<String>();
-		guess.add("blue");
-		guess.add("blue");
-		guess.add("pink");
-		guess.add("red");
-		guess.add("red");
-		guess.add("red");
-		guess.add("red");
-		guess.add("red");
-		System.out.println(code);	
-		System.out.println(guess);	
-		List<Integer> indicators = new ArrayList<Integer>();
-		int rightColourWrongPos = 0;
-		int rightColourRightPos = 0;
-		String used = "used";
-		for (int i=0; i<8; i++) {
-			if (code.contains(guess.get(i))) {
-				counter += 1;
-				rightColourWrongPos++;	
-				indicators.add(1);
-				System.out.println("code colour: " + code.get(i) + ". guess colour: " + guess.get(i));
-				if (guess.get(i).equals(code.get(i))) {
-					indicators.set(i, 2);
-				}
-				for (int j=0; j<8; j++) {
-					if (code.get(j).equals(guess.get(i))) {
-						if (code.get(j).equals(guess.get(j))) {
-							System.out.println("Not too sure what to do now.");		
-						}
-						else {
-							System.out.println("code colour: "+code.get(j)+" guess colour: "+guess.get(i));
-							code.set(j, used);
-							System.out.println("New code is " + code);	
-							break;
-						}
-					}
-				}
-			}
+        List<Integer> indicators = new ArrayList<Integer>();
+        boolean sameColourSamePos;
+		for (int i=0; i<4; i++) {
+            sameColourSamePos = false;
+			if (code.contains(guess.get(i)) && code.get(i).equals(guess.get(i))) { // if right colour in right position
+                indicators.add(2);
+                code.set(i, "used"); // So that later, we give the indicators another 2 for this
+            }
+            else if (code.contains(guess.get(i)) && !code.get(i).equals(guess.get(i))) { // if right colour in wrong position
+                int position = 0;
+                for (int j=0; j<4; j++) {
+                    if (code.get(j).equals(guess.get(i))) { // Find the position in the code where the guess is
+                        position = j;
+                        if (code.get(j).equals(guess.get(j))) { // If there is the correct colour in that position in the guess
+                            sameColourSamePos = true;
+                            indicators.add(2);
+                            code.set(j, "used"); // So that later, we give the indicators another 2 for this
+                            break;
+                        }
+                    }
+                }
+                if (sameColourSamePos == false) {
+                    indicators.add(1);
+                    code.set(position, "used");
+                }
+            }
+            else {
+                indicators.add(0);
+            }
 		}
-		System.out.println("The number of correct colours in the wrong position are " + rightColourWrongPos);
-		System.out.println("The number of correct colours with the correct position are " + rightColourRightPos);
+        Collections.sort(indicators, Collections.reverseOrder());
+        return indicators;
 	}
 }
---
+++ Test.java
@@ -0,0 +1,134 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String args[]) {
+        ArrayList<String> code1 = new ArrayList<String>();
+        code1.add("red");
+        code1.add("red");
+        code1.add("red");
+        code1.add("red");
+        ArrayList<String> guess1 = new ArrayList<String>();
+        guess1.add("red");
+        guess1.add("red");
+        guess1.add("red");
+        guess1.add("red");
+        List<Integer> answer1 = new ArrayList<Integer>();
+        answer1.add(2);
+        answer1.add(2);
+        answer1.add(2);
+        answer1.add(2);
+        if (Checker.check(code1, guess1).equals(answer1)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println();
+
+        ArrayList<String> code2 = new ArrayList<String>();
+        code2.add("red");
+        code2.add("blue");
+        code2.add("green");
+        code2.add("red");
+        ArrayList<String> guess2 = new ArrayList<String>();
+        guess2.add("blue");
+        guess2.add("blue");
+        guess2.add("pink");
+        guess2.add("red");
+        List<Integer> answer2 = new ArrayList<Integer>();
+        answer2.add(2);
+        answer2.add(2);
+        answer2.add(0);
+        answer2.add(0);
+        if (Checker.check(code2, guess2).equals(answer2)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println();
+
+        ArrayList<String> code3 = new ArrayList<String>();
+        code3.add("red");
+        code3.add("blue");
+        code3.add("orange");
+        code3.add("green");
+        ArrayList<String> guess3 = new ArrayList<String>();
+        guess3.add("red");
+        guess3.add("red");
+        guess3.add("red");
+        guess3.add("red");
+        List<Integer> answer3 = new ArrayList<Integer>();
+        answer3.add(2);
+        answer3.add(0);
+        answer3.add(0);
+        answer3.add(0);
+        if (Checker.check(code3, guess3).equals(answer3)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println();
+
+        ArrayList<String> code4 = new ArrayList<String>();
+        code4.add("green");
+        code4.add("red");
+        code4.add("orange");
+        code4.add("blue");
+        ArrayList<String> guess4 = new ArrayList<String>();
+        guess4.add("red");
+        guess4.add("green");
+        guess4.add("blue");
+        guess4.add("orange");
+        List<Integer> answer4 = new ArrayList<Integer>();
+        answer4.add(1);
+        answer4.add(1);
+        answer4.add(1);
+        answer4.add(1);
+        if (Checker.check(code4, guess4).equals(answer4)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println();
+
+        ArrayList<String> code5 = new ArrayList<String>();
+        code5.add("green");
+        code5.add("green");
+        code5.add("orange");
+        code5.add("blue");
+        ArrayList<String> guess5 = new ArrayList<String>();
+        guess5.add("red");
+        guess5.add("green");
+        guess5.add("blue");
+        guess5.add("orange");
+        List<Integer> answer5 = new ArrayList<Integer>();
+        answer5.add(2);
+        answer5.add(1);
+        answer5.add(1);
+        answer5.add(0);
+        if (Checker.check(code5, guess5).equals(answer5)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+
+}
